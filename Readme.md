9085b58451c1a2a24909288d38ea2e8012fb5169
Rohangith1 <107810397+Rohangith1@users.noreply.github.com>
2024-08-05 00:06:56 +0530

    local model

---
+++ Readme.md
@@ -0,0 +1,17 @@
+# Node.js
+
+***what is the difference between Front-End vs Back-End vs database***
+![alt text](maxresdefault.jpg)
+
+download: Node js:  https://nodejs.org/dist/v20.16.0/node-v20.16.0-x64.msi
+
+run : ``` node index.js``` in the terminal
+
+
+***modules***
+
+**1. Local**
+
+**2. in-build**
+
+**3. imported/external**
--- index.js
+++ index.js
@@ -3,12 +3,51 @@
 // }
 
 // hello()
-const addFn=require('./add')
 
-const sum = addFn(2, 3);
-const sum2=addFn(2,5)
+// 2.
+// const addFn=require('./add')
 
-console.log(sum);
-console.log(sum2)
+// const sum = addFn(2, 3);
+// const sum2=addFn(2,5)
 
-console.log("there is nothing")
+// console.log(sum);
+// console.log(sum2)
+
+// console.log("there is nothing")
+
+//
+//path
+
+// const path = require('path')
+// console.log(__filename);
+// console.log(__dirname);
+
+// console.log(path.basename(__filename))
+// console.log(path.basename(__dirname));
+// console.log(path.extname(__filename))
+
+// console.log(path.join('/hello', './hello','file.txt'))
+
+//asyncronus way
+const fs = require('fs');
+// fs.readFile('file.txt', 'utf-8', (err, data) => {
+//     if (err) {
+//         console.log(err)
+//         return
+//     }
+//     console.log(data)
+// })
+
+// const file = fs.readFileSync('file.txt', 'utf-8')
+// console.log(file)
+
+// fs.writeFile('text.txt', 'New file generate', (err) => {
+//     if (err) {
+//         console.log(err)
+//         return
+//     }
+//     console.log("File generated successfully")
+// })
+
+fs.writeFileSync("text2.txt", "New file generate through writeFilesync", "utf-8")
+console.log("File2 generated successfully")
