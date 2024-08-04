63b086f0e5275944b5ed5cccfab30ac727800418
Rohangith1 <107810397+Rohangith1@users.noreply.github.com>
2024-08-04 23:22:40 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// function hello() {
+//     console.log("Hello World")
+// }
+
+// hello()
+const addFn=require('./add')
+
+const sum = addFn(2, 3);
+const sum2=addFn(2,5)
+
+console.log(sum);
+console.log(sum2)
+
+console.log("there is nothing")
